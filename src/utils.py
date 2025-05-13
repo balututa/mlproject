599a0cc6f3b5b46948fb05a42f485d4ea8bf98bd
balu <balu@gmail.com>
2025-05-13 19:33:27 +0530

    Hyperparameter tuning completed

--- src/utils.py
+++ src/utils.py
@@ -5,6 +5,8 @@ import dill
 import numpy as np
 import pandas as pd
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
+from src.logger import logging
 
 import src.exception
 
@@ -21,23 +23,33 @@ def save_object(file_path,obj):
         raise ConnectionRefusedError(e,sys)
     
 
-def evaluate_model(x_train,x_test,y_train,y_test,models):
+from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
+import sys
+from src.exception import CustomException
+
+def evaluate_model(x_train, x_test, y_train, y_test, models, params):
     try:
         report = {}
-        for name,model in models.items():
-            #model = list(model.key())[i]
-            model.fit(x_train,y_train)
 
-            y_train_pred = model.predict(x_train)
-            y_test_pred = model.predict(x_test)
+        for name, model in models.items():
+            model_params = params.get(name, {})  # Safely get params for this model
 
-            y_train_score = r2_score(y_train, y_train_pred)
-            y_test_score = r2_score(y_test, y_test_pred)
+            if model_params:
+                gs = GridSearchCV(model, model_params, cv=3, n_jobs=-1, scoring='r2')
+                gs.fit(x_train, y_train)
+                best_model = gs.best_estimator_
+            else:
+                model.fit(x_train, y_train)
+                best_model = model
+            logging.info(f"Best parameters for {name}: {gs.best_params_}")
 
-            report[name] = y_test_score
+            y_test_pred = best_model.predict(x_test)
+            test_score = r2_score(y_test, y_test_pred)
+
+            report[name] = test_score
 
         return report
 
     except Exception as e:
-        raise ConnectionRefusedError(e,sys)
-        
+        raise CustomException(e, sys)
